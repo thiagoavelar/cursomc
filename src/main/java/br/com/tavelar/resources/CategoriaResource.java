55bf9f0853557372cdfd5c4427565187baa2cb6d
thiagoavelar <tavelars@outlook.com>
2018-06-09 16:25:18 -0300

    categoria domain

--- src/main/java/br/com/tavelar/resources/CategoriaResource.java
+++ src/main/java/br/com/tavelar/resources/CategoriaResource.java
@@ -1,16 +1,23 @@
 package br.com.tavelar.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tavelar.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Está Funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2= new Categoria(2, "Escritório");
+		return Arrays.asList(cat1, cat2);
 	}
 	
 }
